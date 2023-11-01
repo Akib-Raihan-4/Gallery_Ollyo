f893991d0f51a5d90c778957ba88c6c4a1d490a6
Akib-Raihan-4 <raihanakib062@gmail.com>
2023-10-31 20:01:52 -0400

    install tailwind done a small part of nav

---
+++ src/components/nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <>
+        <div className='border border-b-[#a78282] border-transparent'>
+            <h1 className='text-2xl font-bold my-6 ml-10'>
+                Gallery
+            </h1>
+        </div>
+    </>
+  )
+}
+
+export default Nav
